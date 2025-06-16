43784744f387dbf8b1a2f3d2bb5aabf94d20dd41
Ashok Rai <104022510+AshokRai0115@users.noreply.github.com>
2025-06-16 22:08:19 +0545

    user schema and post request

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,10 @@
-// import {Admin} from '../models/authModel';
-// import jwt from 'jsonwebtoken';
+
 const AdminSchema = require("../models/authModel")
 module.exports.adminSignup = async(req, res)=>{
     const{email, password, role} = req.body;
+    console.log(email, password)
     try{
-        const user = await AdminSchema.create(email, password, role);
+        const user = await AdminSchema.create({email, password, role});
         
         res.status(201).json({
             msg: 'User created successfully'})
@@ -13,5 +13,17 @@ module.exports.adminSignup = async(req, res)=>{
         res.status(405).json({
             msg: `Failed to create user ${error}`})
 
+    }
+}
+
+module.exports.adminLogin = async(req, res)=>{
+    try{
+
+        const{username, password} = req.body;
+        const loginSuccess = await AdminSchema.adminLogin
+    }catch(error){
+        res.status(403).json({
+            msg: `Failed to login user: ${error}`
+        })
     }
 }
